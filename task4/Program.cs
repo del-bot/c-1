bbd0d1044c4ef613a0d1aa8269450f59b21f83db
Delyash <delyash91@mail.ru>
2022-11-24 00:56:34 +0600

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,22 @@
+﻿int max = 0;
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine(" Максимальное число = " +max);
