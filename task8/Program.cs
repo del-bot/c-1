8533408fd3e84275ff72a3a201dfd3e940ea94ab
Delyash <delyash91@mail.ru>
2022-11-24 01:10:28 +0600

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int P;
+for (P=1; P < N; P++ )
+{
+    if (P % 2 == 0)
+    {
+        Console.WriteLine(+P);
+    }
+}
+Console.WriteLine(N);
